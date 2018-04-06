e78444a552d873e9b7a51273af7c93e077f9d425
Michael Boroski <mjboroski@gmail.com>
2018-04-06 18:57:09 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,12 +3,10 @@ def display_board
   cell="   "
   pipe="|"
   divider="-----------"
-  def display_line
-    d_line=cell+pipe+cell+pipe+cell
-    puts d_line
-  end
-  def display_divider
-    puts divider
-  end
-  return display_line+display_divider+display_line+display_divider+display_line
+  d_line=cell+pipe+cell+pipe+cell
+  puts d_line
+  puts divider
+  puts d_line
+  puts divider
+  puts d_line
 end
